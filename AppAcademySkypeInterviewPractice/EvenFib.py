e93ccf8cf096c294450c24fdc9d83f00270d50ed
Jstuff36 <mizzou_fan10@hotmail.com>
2016-11-19 09:55:34 -0600

    Created variable to allow for changing upper limit

--- AppAcademySkypeInterviewPractice/EvenFib.py
+++ AppAcademySkypeInterviewPractice/EvenFib.py
@@ -1,6 +1,7 @@
 def fib_series():
 	terms = [0,1]
-	while(terms[-1] + terms[-2] < 4000000):
+	num = 4000000 #change this term to change the upper limit of the sequence
+	while(terms[-1] + terms[-2] < num):
 		terms.append(terms[-1]+terms[-2])
 	return(sum(i for i in terms if i%2 == 0))
 
