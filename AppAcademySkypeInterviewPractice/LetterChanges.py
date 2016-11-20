c7918edc0b5a55b2198467949c0ab876aa1870ec
Jstuff36 <mizzou_fan10@hotmail.com>
2016-11-20 16:52:30 -0600

    Changing all letters in a string by 1 character

---
+++ AppAcademySkypeInterviewPractice/LetterChanges.py
@@ -0,0 +1,43 @@
+test_string = 'hello*3'
+
+def LetterChanges(test_string):
+	new_string = ''
+	for i in range(len(test_string)):
+		new_string += dic(test_string[i])
+	return(new_string)
+
+def dic(char):
+	dictionary ={
+		'a': 'b',
+		'b': 'c',
+		'c': 'd',
+		'd': 'E',
+		'e': 'f',
+		'f': 'g',
+		'g': 'h',
+		'h': 'I',
+		'i': 'j',
+		'j': 'k',
+		'k': 'l',
+		'l': 'm',
+		'm': 'n',
+		'n': 'O',
+		'o': 'p',
+		'p': 'q',
+		'q': 'r',
+		'r': 's',
+		's': 't',
+		't': 'U',
+		'u': 'v',
+		'v': 'w',
+		'w': 'x',
+		'x': 'y',
+		'y': 'z',
+		'z': 'A'
+		}
+	if char in dictionary:
+		return(dictionary[char])
+	else:
+		return(char)
+
+print(LetterChanges(test_string))
