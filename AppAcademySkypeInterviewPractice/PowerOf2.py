29e4b6bec0996f0200c2200d65e90a611be6992a
Jstuff36 <mizzou_fan10@hotmail.com>
2016-11-22 22:47:41 -0600

    Better way of solving the problem. Does not use recursion

--- AppAcademySkypeInterviewPractice/PowerOf2.py
+++ AppAcademySkypeInterviewPractice/PowerOf2.py
@@ -1,6 +1,7 @@
-num = 0
+num = 18
 
 def PowerOf2(num):
+	#catch if 0 is passed in
 	if num == 0:
 		return('false')
 	if num == 1:
@@ -11,3 +12,15 @@ def PowerOf2(num):
 		return(PowerOf2(num/2))
 
 print(PowerOf2(num))
+
+#better way of doing it because recursion in python is very memory intensive
+def PowerOf2NonRecursive(num):
+	if num == 0:
+		return('false')
+	while num > 1:
+		if int(num/2) != num/2:
+			return('false')
+		num /= 2
+	return('true')
+
+print(PowerOf2NonRecursive(num))
