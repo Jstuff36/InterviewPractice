2a5da80382b7db076cb23ffee269beaa7ef2719b
Jstuff36 <mizzou_fan10@hotmail.com>
2016-11-29 23:18:34 -0600

    added cool way with list comprehension

--- AppAcademySkypeInterviewPractice/disemvowel.py
+++ AppAcademySkypeInterviewPractice/disemvowel.py
@@ -9,3 +9,20 @@ def disemvowel(test_string):
 	return(''.join(ans))
 
 print(disemvowel(test_string))
+
+def disemvowel2(test_string):
+	Vowels = 'aeiou'
+	ans = ''
+	for char in test_string:
+		if char.lower() not in Vowels:
+			ans += char
+	return(ans)
+
+print(disemvowel2(test_string))
+
+def listComp(test_string):
+	vowels = 'aeiou'
+	ans = [char for char in test_string if char.lower() not in vowels]
+	return(''.join(ans))
+
+print(listComp(test_string))
