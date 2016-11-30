88f627e29276b20f14f75a689821b1695947e65d
Jstuff36 <mizzou_fan10@hotmail.com>
2016-11-30 15:26:36 -0600

    Small change to increase efficiency (ironic)

--- AppAcademySkypeInterviewPractice/BubbleSort.py
+++ AppAcademySkypeInterviewPractice/BubbleSort.py
@@ -2,7 +2,7 @@ test_list = [5,6,2,3]
 
 def bubble_sort(test_list):
 	for i in range(len(test_list)):
-		for j in range(i,len(test_list)):
+		for j in range(i+1,len(test_list)):
 			if test_list[i] > test_list[j]:
 				temp = test_list[i]
 				test_list[i] = test_list[j]
