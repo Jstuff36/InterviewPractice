58f0b9baf6a6debf55dcb128db9058ae8f628262
Jstuff36 <mizzou_fan10@hotmail.com>
2017-02-21 13:22:03 -0600

    Added edgecase when number is less than 2

--- AppAcademySkypeInterviewRuby/is_prime?.rb
+++ AppAcademySkypeInterviewRuby/is_prime?.rb
@@ -1,4 +1,5 @@
 def is_prime?(num)
+	return false if num < 2 #only numbers greater than 1 can be prime
 	return true if num == 2 #only even prime
 	(2...num).each do |i|
 		return false if num % i == 0
