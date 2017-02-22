f60e4728744abe061c520306b9c50be3c67c0c1c
Jstuff36 <mizzou_fan10@hotmail.com>
2017-02-22 16:00:16 -0600

    Cool alternative

--- AppAcademySkypeInterviewRuby/greatest_common_factor.rb
+++ AppAcademySkypeInterviewRuby/greatest_common_factor.rb
@@ -14,3 +14,19 @@ def greatest_common_factor(num1, num2)
 end
 
 p greatest_common_factor(3, 5)
+
+def AA_greatest_common_factor(num1, num2)
+
+	if num1 > num2
+		i = num1
+	else
+		i = num2
+	end
+
+	while true
+		return i if num1 % i == 0 and num2 % i == 0
+		i -= 1
+	end
+end
+
+p AA_greatest_common_factor(18, 24)
