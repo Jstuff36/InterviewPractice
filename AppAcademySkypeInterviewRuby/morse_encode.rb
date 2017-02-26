fba8d30c8587c6778686f1061d44df68ade799a0
Jstuff36 <mizzou_fan10@hotmail.com>
2017-02-26 15:06:36 -0600

    Translates a string into morse code

---
+++ AppAcademySkypeInterviewRuby/morse_encode.rb
@@ -0,0 +1,48 @@
+MORSE_CODE = {
+  "a" => ".-",
+  "b" => "-...",
+  "c" => "-.-.",
+  "d" => "-..",
+  "e" => ".",
+  "f" => "..-.",
+  "g" => "--.",
+  "h" => "....",
+  "i" => "..",
+  "j" => ".---",
+  "k" => "-.-",
+  "l" => ".-..",
+  "m" => "--",
+  "n" => "-.",
+  "o" => "---",
+  "p" => ".--.",
+  "q" => "--.-",
+  "r" => ".-.",
+  "s" => "...",
+  "t" => "-",
+  "u" => "..-",
+  "v" => "...-",
+  "w" => ".--",
+  "x" => "-..-",
+  "y" => "-.--",
+  "z" => "--.."
+}
+
+def morse_encode(str)
+	words = str.split(" ")
+	encoded = []
+	words.each do |word|
+		encoded << morse_encode_word(word)
+	end
+
+	encoded.join("  ")
+
+end
+
+def morse_encode_word(word)
+	morse = word.split("").map do |let|
+		MORSE_CODE[let]
+	end
+	morse.join(" ")
+end
+
+p morse_encode("cat in hat")
