49eece6161260f70865d85e19f8779c6adef3cf5
Jstuff36 <mizzou_fan10@hotmail.com>
2017-02-27 16:50:20 -0600

    Added a recursive solution to pascal's triangle

--- AppAcademySkypeInterviewRuby/pascals_triangle.rb
+++ AppAcademySkypeInterviewRuby/pascals_triangle.rb
@@ -14,4 +14,22 @@ def pascals_triangle(levels, arr = [[1], [1,1]])
 	arr
 end
 
-p pascals_triangle(10)
+p pascals_triangle(5)
+
+def pascals_triangle_rec(levels)
+	return [[1]] if levels == 0
+	return [[1],[1,1]] if levels == 1
+
+	result = pascals_triangle_rec(levels - 1)
+	new_row = [1]
+
+	last_row = result[-1]
+	(0...last_row.length-1).each do |i|
+		new_row << last_row[i] + last_row[i+1]
+	end
+	new_row << 1
+	result << new_row
+	result
+end
+
+p pascals_triangle_rec(5)
