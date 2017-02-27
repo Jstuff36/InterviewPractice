aad716a0ba49ad5a8d2afe066375a6cada28a490
Jstuff36 <mizzou_fan10@hotmail.com>
2017-02-27 12:17:20 -0600

    Added base case

--- AppAcademySkypeInterviewRuby/fibs.rb
+++ AppAcademySkypeInterviewRuby/fibs.rb
@@ -1,16 +1,18 @@
 def fibs(num)
+	return 1 if num < 2
 	arr = [0, 1]
 	(1..num-2).each { |i| arr << arr[-1] + arr[-2] }
 	arr
 end
 
-p fibs(5)
+p fibs(2)
 
 def fibs_rec(num, arr = [0, 1])
+	return 1 if num < 2
 	return arr if num == 2
 	arr << arr[-1] + arr[-2]
 	fibs_rec(num-1, arr)
 
 end
 
-p fibs_rec(5)
+p fibs_rec(2)
